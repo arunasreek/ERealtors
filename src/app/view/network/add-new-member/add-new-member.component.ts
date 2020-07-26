d97c3605e3da5c9f475790630d94d83d5f5ce9f1
devendra300 <devendrakoppu91221@gmail.com>
2020-07-26 14:29:11 +0530

    done validation parts

--- src/app/view/network/add-new-member/add-new-member.component.ts
+++ src/app/view/network/add-new-member/add-new-member.component.ts
@@ -6,6 +6,8 @@ import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { combineLatest, Subscription } from 'rxjs';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-new-member',
@@ -24,7 +26,9 @@ export class AddNewMemberComponent implements OnInit {
   Guid : string;
   events: Subscription[] = [];
   messages: string[] = [];
-  constructor(private modalService: BsModalService,public memberService: MemberServices,
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private modalService: BsModalService,public memberService: MemberServices,private formBuilder: FormBuilder,
               public toastr: ToastrService,private changeDetection: ChangeDetectorRef) { }
   model = {
            RefId :0,
@@ -49,6 +53,7 @@ export class AddNewMemberComponent implements OnInit {
            Aadhaar_Number :'',
            Bank_Name :'',
            IFSC_Code :'',
+           Admin:0,
            Bank_Account_Number :'',
            IsOptingforStar1Autopool :0,
            UpgradeAmountPaid :0,
@@ -59,39 +64,77 @@ export class AddNewMemberComponent implements OnInit {
     this.getSponserList();
     this.getMemberList();
     this.Guid = null;
+    this.registerForm = this.formBuilder.group({
+      First_Name: ['', [Validators.required, Validators.minLength(3)]],
+      Surname: ['',Validators.required],
+      Gender: ['Male',Validators.required],
+      UserName: ['',[Validators.required, Validators.minLength(6)]],
+      Email_Address: ['', [Validators.required, Validators.email]],
+      Password: ['', [Validators.required, Validators.minLength(6)]],
+      Confirm_Password: ['', Validators.required],
+      Mobile_Number: ['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      Sponsor :['', Validators.required],
+      Name_of_Nominee:[''],
+      Date_of_Joining : [new  Date()],
+      Date_of_Birth  :   [new  Date()],
+      address: ['', Validators.required],
+      Pan_Card_Number: ['', Validators.required],
+      Aadhaar_Number: ['', Validators.required],
+      Bank_Name: ['', Validators.required],
+      IFSC_Code: ['', Validators.required],
+      Bank_Account_Number: ['', Validators.required],
+      plot_sqyds: [0, Validators.required],
+      no_of_plots: [0, Validators.required],
+      rate_per_plot: [0, Validators.required],
+      IsOptingforStar1Autopool: [0, Validators.required],
+      Admin: [0],
+      UpgradeAmountPaid: [0, Validators.required],
+      perks: ['', Validators.required]
+      
+  }, {
+  
+  });
+   
   }
-
+  get f() { return this.registerForm.controls; }
+  
   pushMember(){
+    this.submitted = true;
+    console.log(this.registerForm.value);
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
     const data = {
       ActionTaken : this.Guid?'Update':'Insert',
       Id : this.model.Id,
-      RefId :parseInt(this.model.RefId.toString()),
-      plot_sqyds : parseInt(this.model.plot_sqyds.toString()),
-      no_of_plots : parseInt(this.model.no_of_plots.toString()),
-      rate_per_plot : parseInt(this.model.rate_per_plot.toString()),
-      address  :this.model.address,
-      Country :this.model.Country,
-      First_Name :this.model.First_Name,
-      Surname  :this.model.Surname,
-      Username :this.model.Username,
-      Email_Address :this.model.Email_Address,
-      Date_of_Joining : this.model.Date_of_Joining,
-      Date_of_Birth  :   this.model.Date_of_Birth,
-      perks  :this.model.perks,
-      Password :this.model.Password,
-      Confirm_Password :this.model.Confirm_Password,
-      Sponsor : parseInt(this.model.Sponsor),
-      Name_of_Nominee :this.model.Name_of_Nominee,
-      Mobile_Number :this.model.Mobile_Number,
-      Pan_Card_Number :this.model.Pan_Card_Number,
-      Aadhaar_Number :this.model.Aadhaar_Number,
-      Bank_Name :this.model.Bank_Name,
-      IFSC_Code :this.model.IFSC_Code,
-      Bank_Account_Number :this.model.Bank_Account_Number,
-      IsOptingforStar1Autopool : this.model.IsOptingforStar1Autopool?1:0,
-      UpgradeAmountPaid :this.model.UpgradeAmountPaid,
+      RefId :0,
+      plot_sqyds : parseInt(this.registerForm.value.plot_sqyds.toString()),
+      no_of_plots : parseInt(this.registerForm.value.no_of_plots.toString()),
+      rate_per_plot : parseInt(this.registerForm.value.rate_per_plot.toString()),
+      address  :this.registerForm.value.address,
+      Country :this.registerForm.value.Country,
+      First_Name :this.registerForm.value.First_Name,
+      Surname  :this.registerForm.value.Surname,
+      Username :this.registerForm.value.Username,
+      Email_Address :this.registerForm.value.Email_Address,
+      Date_of_Joining : this.registerForm.value.Date_of_Joining,
+      Date_of_Birth  :   this.registerForm.value.Date_of_Birth,
+      perks  :this.registerForm.value.perks,
+      Password :this.registerForm.value.Password,
+      Confirm_Password :this.registerForm.value.Confirm_Password,
+      Sponsor : parseInt(this.registerForm.value.Sponsor),
+      Name_of_Nominee :this.registerForm.value.Name_of_Nominee,
+      Mobile_Number :this.registerForm.value.Mobile_Number,
+      Pan_Card_Number :this.registerForm.value.Pan_Card_Number,
+      Aadhaar_Number :this.registerForm.value.Aadhaar_Number,
+      Bank_Name :this.registerForm.value.Bank_Name,
+      IFSC_Code :this.registerForm.value.IFSC_Code,
+      Bank_Account_Number :this.registerForm.value.Bank_Account_Number,
+      IsOptingforStar1Autopool : this.registerForm.value.IsOptingforStar1Autopool?1:0,
+      UpgradeAmountPaid :this.registerForm.value.UpgradeAmountPaid,
       ImageUrl : this.imageUrl,
-      Gender:this.model.Gender
+      Gender:this.registerForm.value.Gender
     };
     this.memberService.postMember(data)
     .subscribe(() => {
